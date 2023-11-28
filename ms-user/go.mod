5ba6d324d74e3355c9a8e48eed0c9761f029ced9
Tan Karunia Dzikra <tankaruniadzikra@gmail.com>
2023-11-28 16:48:45 +0700

    update register user

--- ms-user/go.mod
+++ ms-user/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.11.3
 	golang.org/x/crypto v0.14.0
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	gorm.io/driver/postgres v1.5.4
 	gorm.io/gorm v1.25.5
 )
@@ -26,4 +27,5 @@ require (
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 )
--- ms-user/routes/routes.go
+++ ms-user/routes/routes.go
@@ -9,14 +9,16 @@ import (
 
 func RegisterRoutes(e *echo.Echo) {
 
-	e.POST("/users/register", handler.RegisterUser)
-	e.POST("/users/login", handler.LoginUser)
+	e.POST("/register", handler.RegisterUser)
+	e.POST("/login", handler.LoginUser)
 
-	e.POST("/users/profile/create", handler.CreateUserProfile, middleware.RequireAuth)
-	e.GET("/users/profile", handler.GetUserProfile, middleware.RequireAuth)
-	e.PUT("/users/profile/update", handler.UpdateUserProfile, middleware.RequireAuth)
+	user := e.Group("/user")
+	user.Use(middleware.RequireAuth)
+	{
+		user.GET("/profile", handler.GetUserProfile)
+		user.PUT("/profile/update", handler.UpdateUserProfile)
+		user.GET("/activities", handler.GetUserActivities)
+	}
 
-	e.GET("/users/activities", handler.GetUserActivities, middleware.RequireAuth)
-
-	e.GET("/admin/users/:id/activities", handler.GetActivitiesByUserID, middleware.AdminOnlyMiddleware)
+	e.GET("/admin/:id/activities", handler.GetActivitiesByUserID, middleware.AdminOnlyMiddleware)
 }
